27d0a16639a651e0b74ceb815b65daea70052cfa
Dracrius <a.achorn@gmail.com>
2021-05-19 13:10:23 -0400

    Fixed the Dpad not working due to XRInputManager
    Stripped everything back to just the Hold PDA to pause code as everything else was unneeded and broke controller input for many users.

---
+++ VRTweaks/HoldPDAtoPause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using HarmonyLib;
+using System;
+using Platform.Utils;
+using FMODUnity;
+using UnityEngine.Events;
+
+namespace VRTweaks
+{
+        [HarmonyPatch(typeof(FPSInputModule), nameof(FPSInputModule.EscapeMenu))]
+        class FPSInputModule_EscapeMenu_Patch
+        {
+            [HarmonyPrefix]
+            static void Prefix(FPSInputModule __instance)
+            {
+                if (__instance.lockPauseMenu)
+                {
+                    return;
+                }
+                //Press and hold pda to access escape menu with touch controllers (should be left controller menu button by default)
+                if (GameInput.GetButtonDown(GameInput.Button.UIMenu) && IngameMenu.main != null && !IngameMenu.main.selected || GameInput.GetButtonHeldTime(GameInput.Button.PDA) > 1.0f)
+                {
+                    IngameMenu.main.Open();
+                    GameInput.ClearInput();
+                }
+            }
+        }
+}
