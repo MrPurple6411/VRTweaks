3fdd678e6f4b916487db34aeb04d3c9d121e6142
EthanFischer <ethan.f.fischer@gmail.com>
2021-05-26 22:22:40 -0500

    Make crafting menu work

--- VRTweaks/GamepadGazeCursorFixer.cs
+++ VRTweaks/GamepadGazeCursorFixer.cs
@@ -1,28 +1,14 @@
 ﻿using HarmonyLib;
-using UnityEngine.XR;
 
 namespace VRTweaks
 {
-
-    [HarmonyPatch(typeof(VROptions), "GetUseGazeBasedCursor")]
-    public static class AlternateGazeCursor
-    {
-        [HarmonyPrefix]
-        public static bool Prefix(ref bool __result)
-        {
-			__result = (VROptions.gazeBasedCursor || SnapTurningOptions.GamepadGazeCursor) && XRSettings.enabled;
-			return false;
-        }
-    }
-
-
     [HarmonyPatch(typeof(IngameMenu), "UpdateRaycasterStatus")]
-    public static class PauseMenu
+    public static class PauseMenuRaycaster
     {
         [HarmonyPrefix]
         public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, IngameMenu __instance)
         {
-            if (GameInput.IsPrimaryDeviceGamepad() && !SnapTurningOptions.GamepadGazeCursor)
+            if (GameInput.IsPrimaryDeviceGamepad() && !VROptions.GetUseGazeBasedCursor())
             {
                 raycaster.enabled = false;
             }
@@ -36,12 +22,12 @@ namespace VRTweaks
     }
 
     [HarmonyPatch(typeof(uGUI_PDA), "UpdateRaycasterStatus")]
-    public static class PDAMenu
+    public static class PDAMenuRaycaster
     {
         [HarmonyPrefix]
         public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, uGUI_PDA __instance)
         {
-            if (GameInput.IsPrimaryDeviceGamepad() && !SnapTurningOptions.GamepadGazeCursor)
+            if (GameInput.IsPrimaryDeviceGamepad() && !VROptions.GetUseGazeBasedCursor())
             {
                 raycaster.enabled = false;
             }
@@ -55,95 +41,67 @@ namespace VRTweaks
     }
 
 
-    //[HarmonyPatch(typeof(uGUI_CraftingMenu), "SetRaycasterStatus")]
-    //public static class CraftingMenuRaycaster
-    //{
-    //    [HarmonyPrefix]
-    //    public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, uGUI_CraftingMenu __instance)
-    //    {
-    //        if (GameInput.IsPrimaryDeviceGamepad() && !SnapTurningOptions.GamepadGazeCursor)
-    //        {
-    //            raycaster.enabled = false;
-    //        }
-    //        else
-    //        {
-    //            raycaster.enabled = __instance.focused;
-    //        }
+    [HarmonyPatch(typeof(uGUI_CraftingMenu), "SetRaycasterStatus")]
+    public static class CraftingMenuRaycaster
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, uGUI_CraftingMenu __instance)
+        {
+            if (GameInput.IsPrimaryDeviceGamepad() && !VROptions.GetUseGazeBasedCursor())
+            {
+                raycaster.enabled = false;
+            }
+            else
+            {
+                raycaster.enabled = __instance.focused;
+            }
 
-    //        return false;
-    //    }
-    //}
+            return false;
+        }
+    }
 
+    [HarmonyPatch(typeof(uGUI_CraftingMenu), "uGUI_IIconManager.OnPointerClick")]
+    public static class CraftingMenuOnPointerClick
+    {
+        private static bool _didSpoofKeyboard;
 
-	//[HarmonyPatch(typeof(uGUI_CraftingMenu), "SetRaycasterStatus")]
-	//public static class CraftingMenuPointerBehavior
-	//{
- //       [HarmonyPrefix]
-	//	public static bool Prefix(ref uGUI_ItemIcon icon, ref int button, uGUI_CraftingMenu __instance bool __result)
-	//	{
-	//		bool flag = GameInput.GetPrimaryDevice() == GameInput.Device.Controller;
-	//		if (!__instance.locked)
-	//		{
-	//			uGUI_CraftingMenu.Node node = __instance.GetNode(icon);
-	//			if (node != null)
-	//			{
-	//				if (flag)
-	//				{
-	//					switch (button)
-	//					{
-	//						case 0:
-	//							if (node.action == TreeAction.Craft)
-	//							{
-	//								__instance.Action(node);
-	//							}
-	//							else
-	//							{
-	//								((uGUI_INavigableIconGrid)__instance.SelectItemInDirection(1, 0);
-	//							}
-	//							break;
-	//						case 1:
-	//							__instance.Out(node.parent as uGUI_CraftingMenu.Node);
-	//							break;
-	//						case 2:
-	//							if (node.action == TreeAction.Craft)
-	//							{
-	//								TechType techType = node.techType;
-	//								if (CrafterLogic.IsCraftRecipeUnlocked(techType))
-	//								{
-	//									PinManager.TogglePin(techType);
-	//								}
-	//							}
-	//							break;
-	//					}
-	//				}
-	//				else if (button != 0)
-	//				{
-	//					if (button == 1)
-	//					{
-	//						if (node.action == TreeAction.Craft)
-	//						{
-	//							TechType techType2 = node.techType;
-	//							if (CrafterLogic.IsCraftRecipeUnlocked(techType2))
-	//							{
-	//								PinManager.TogglePin(techType2);
-	//							}
-	//						}
-	//					}
-	//				}
-	//				else
-	//				{
-	//					__instance.Action(node);
-	//				}
-	//			}
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            if (VROptions.GetUseGazeBasedCursor())
+            {
+                GameInputSpoofer.ShouldSpoofKeyboard = true;
+                _didSpoofKeyboard = true;
+            }
 
-	//			return true;
-	//		}
-	//		if (flag && button == 1)
-	//		{
-	//			__instance.Deselect(null);
-	//			return true;
-	//		}
-	//		return false;
-	//	}
-	//}
+            return true;
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if (_didSpoofKeyboard)
+            {
+                GameInputSpoofer.ShouldSpoofKeyboard = false;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GameInput), nameof(GameInput.GetPrimaryDevice))]
+    public static class GameInputSpoofer
+    {
+        public static bool ShouldSpoofKeyboard { get; set; }
+
+        [HarmonyPrefix]
+        public static bool Prefix(ref GameInput.Device __result)
+        {
+            if(ShouldSpoofKeyboard)
+            {
+                __result = GameInput.Device.Keyboard;
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
