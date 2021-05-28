6580669313af9d8005ac5b01fd82adb8739b70b9
EthanFischer <ethan.f.fischer@gmail.com>
2021-05-28 18:17:10 -0500

    Separate classes into files

---
+++ VRTweaks/GamepadGazeCursorFixer/PDAMenu.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using VRTweaks.Utilities.GamepadGazeCursorFixer;
+
+namespace VRTweaks.GamepadGazeCursorFixer
+{
+    [HarmonyPatch(typeof(uGUI_PDA), "UpdateRaycasterStatus")]
+    public static class PDAMenuRaycaster
+    {
+        public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, uGUI_PDA __instance)
+        {
+            RaycastFixer.Fix(ref raycaster, __instance);
+            return false;
+        }
+    }
+}
