6173394dc6e69a13591b899b87a65d7e43b091d9
EthanFischer <ethan.f.fischer@gmail.com>
2021-05-30 18:23:54 -0500

    Quick slot working with gaze/nongaze!

--- VRTweaks/GamepadGazeCursorFixer/PDAMenu.cs
+++ VRTweaks/GamepadGazeCursorFixer/PDAMenu.cs
@@ -12,4 +12,38 @@ namespace VRTweaks.GamepadGazeCursorFixer
             return false;
         }
     }
+
+
+    static class PDAQuickSlotAssignment
+    {
+        private static bool _didDisableGazeCursor;
+
+        [HarmonyPatch(typeof(uGUI_InventoryTab), nameof(uGUI_InventoryTab.OnUpdate))]
+        public static class PDAQuickSlotAssignmentBegin
+        {
+            public static void Postfix()
+            {
+                if (VROptions.GetUseGazeBasedCursor() && GameInput.GetButtonDown(uGUI.button3))
+                {
+                    Inventory.main.ExecuteItemAction(ItemDragManager.hoveredItem, 3);
+                    VROptions.gazeBasedCursor = false;
+                    _didDisableGazeCursor = true;
+                }
+            }
+        }
+
+
+        [HarmonyPatch(typeof(QuickSlots), nameof(QuickSlots.EndAssign))]
+        public static class PDAQuickSlotAssignmentEnd
+        {
+            public static void Postfix()
+            {
+                if (_didDisableGazeCursor)
+                {
+                    VROptions.gazeBasedCursor = true;
+                }
+            }
+        }
+
+    }
 }
